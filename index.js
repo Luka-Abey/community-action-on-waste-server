c113521f080ee35d31922986397dc9665fe6f77a
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-01-31 15:21:00 +0000

    trying to sort cors

--- index.js
+++ index.js
@@ -18,13 +18,7 @@ mongoose.set('useUnifiedTopology', true)
 
 
 // // Avoid CORS error
-app.use(
-  cors({
-    // origin: url,
-    // credentials: true
-    // exposedHeaders: ['set-cookie']
-  })
-)
+app.use(cors({ origin: true }))
 
 // BodyParser
 app.use(express.json())
